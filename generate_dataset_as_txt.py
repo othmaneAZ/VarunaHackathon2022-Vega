a19cf96084fe81ebbb0e0b9fe8c92c66b67decf4
Abid Ali <aali.abidkhan@gmail.com>
2022-06-19 18:40:15 +0200

    to prodvide info about label 0

--- generate_dataset_as_txt.py
+++ generate_dataset_as_txt.py
@@ -8,6 +8,9 @@ def generate_txt(dir_path, output_txt):
        label = element
        element_path = dir_path + '/' + element_0
        num_frames = len(os.listdir(element_path))
-       f.write(element_path + ' ' + str(num_frames) + ' ' + '0' + '\n')
+       f.write(element_path + ' ' + str(num_frames) + ' ' + '0' + '\n') 
+      '''
+      we dont have a lebel, so we will use a dummy label aka 0 to make the file compatible to mmaction input.
+      '''
     
 generate_txt('/data/stars/user/mguermal/varuna/frames_varuna_test','/data/stars/user/mguermal/varuna/anno_test_file.txt')
